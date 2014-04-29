1680e4a13f96032a6c75a018847d8c880af2e6cf
Norberto <naroldan@pcnaroldan.local>
2014-04-29 18:16:05 -0500

    Listo

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>NARB</title>
+		<meta charset="UTF-8">
+		<!--<link href="css/estilo.css" type="text/css" rel="stylesheet"/>-->
+		<!--<script src="js/jquery-1.8.2.js"></script>-->
+	</head>
+	<body>
+	</body>
+</html>
